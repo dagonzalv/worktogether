8b3f9ba544d7f475b3f6de9f1649fd5e5e38177e
Andres Aquino <aquino@hp.com>
2011-02-10 18:55:43 -0600

    TASK: estructura base de rails3, este sera el baseline

---
+++ source/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Source::Application.config.secret_token = '0a9da2d3766abf08ef4f3729a64af9305d5e1930f9a22cdb797865871d76ff62834cd85c7f13357352b267fb9f3536e9fd02501d06a76cdace94eefde05b872f'
